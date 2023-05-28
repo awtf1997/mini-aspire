f00b5df70e09dfdb1048f0bad3f5064463c5e31e
awtf1997 <85084841+awtf1997@users.noreply.github.com>
2023-05-28 11:14:01 +0530

    integration tests for loan flow

--- mini-aspire-user-loans-app/src/test/java/com/aspire/miniaspireuserloansapp/MiniAspireUserLoansAppApplicationTests.java
+++ mini-aspire-user-loans-app/src/test/java/com/aspire/miniaspireuserloansapp/MiniAspireUserLoansAppApplicationTests.java
@@ -1,9 +1,7 @@
 package com.aspire.miniaspireuserloansapp;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class MiniAspireUserLoansAppApplicationTests {
 
 	@Test
